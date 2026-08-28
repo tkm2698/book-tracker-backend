1bcc5b6956381991cb0a3f2a64baf2c05bc55507
Trevon Murtaza <trevon.murtaza@pointpark.edu>
2026-08-27 20:26:26 -0400

    enable cors

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const { Pool } = require("pg");
+const cors = require("cors");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.json());
 
 const pool = process.env.DATABASE_URL
